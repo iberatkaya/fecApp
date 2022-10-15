318e4fecb5e5aef76fc987fd5f75f99353320615
iberatkaya <beratkaya1998@gmail.com>
2022-10-15 18:46:27 -0500

    Initial Commit

---
+++ fecApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fecApp
+//
+//  Created by Ibrahim Berat Kaya on 10/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
